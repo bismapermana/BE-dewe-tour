16d22c63465fa74e2ed8ae28b4428d63c24f6803
bismapermana <bismapermana@gmail.com>
2021-11-15 10:16:00 +0700

    core done

--- server.js
+++ server.js
@@ -4,9 +4,19 @@ const express = require("express");
 
 const cors = require("cors");
 
+const http = require("http");
+const { Server } = require("socket.io");
+
 const router = require("./src/routes/index");
 
 const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
 
 const port = 5000;
 
@@ -15,9 +25,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/uploads", express.static("uploads"));
-
 app.use("/api/v1/", router);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Listening on port ${port}!`);
 });
